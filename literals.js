787496b46ad05b6b7e078ea28988969f6ed0c224
Aatika Hakim <aatikahakim90@gmail.com>
2022-07-19 13:26:12 -0400

    strings

---
+++ literals.js
@@ -0,0 +1,21 @@
+let user = "Aatika"
+let marks = "350";
+let hello = `Hello ${user} your marks are ${marks}`;
+
+document.write(hello);
+document.write("<br><br>");
+
+// function
+
+let firstName = "Aatika";
+let lastName = "Hakim";
+
+function fullName(){
+    return `${firstName} ${lastName}`;
+}
+
+let Hello = `${firstName} ${lastName}`;
+
+document.write(Hello);
+
+
