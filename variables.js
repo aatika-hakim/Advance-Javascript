4cad0ef7b3add1f281e2b069aa1a71d85d387157
Aatika Hakim <aatikahakim90@gmail.com>
2022-07-19 13:25:44 -0400

    stings& variables

---
+++ variables.js
@@ -0,0 +1,32 @@
+// var variable
+/* var can be declared again
+value can be reasigned to var 
+the scope of var is global */
+
+var a = "Hello";
+var a = "World";
+a = "Wow";
+
+document.write(a);
+
+// let variable
+/* let cannot be declared again
+value can be reasigned to var 
+the scope of let is block */
+
+let a = "Hello";
+let a = "World";
+a = "Wow";
+
+document.write(a);
+
+// const variable
+/* const cannot be declared again
+value cannot be reasigned to var 
+the scope of let is block */
+
+const a = "Hello";
+const a = "World";
+a = "Wow";
+
+document.write(a);
